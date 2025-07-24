93ca8544f49d8fd3911b8122918c77071393da23
Peisinoe2427 <melisa.lauriola@student.howest.be>
2025-07-24 13:55:46 +0200

    finished big door animation and some gsap

--- src/main.js
+++ src/main.js
@@ -6,12 +6,29 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 import { TextPlugin } from "gsap/TextPlugin";
 
-// INK LANGUGAGES GAMES
+let tapCount = 0;
+let tapTimer = null;
+const closedDoor = document.querySelector(".js-door-closed");
+const openDoor = document.querySelector(".js-door-open");
+const closedDoorImg = closedDoor?.querySelector("img");
+const sectionP1 = document.querySelector(".ch5__p1");
+const crownImg = document.querySelector(".img_crown");
+const textBlock = document.querySelector(".ch5__text");
+
 const resetFeedback= (message, type = 'neutral') =>{
     const feedback = document.querySelector('.feedback');
     feedback.innerHTML = message;
     feedback.className = `feedback feedback--${type}`;
 }
+
+const setDisplayResponsive = (el, largeDisplay = "grid", smallDisplay = "block") => {
+    const isLargeScreen = window.matchMedia("(min-width: 41em)").matches;
+    el.style.display = isLargeScreen ? largeDisplay : smallDisplay;
+};
+
+// INK LANGUGAGES GAMES
+
+
 
 const resetInkSelection =()=> {
     const inkOptions = document.querySelectorAll('.ink-option');
@@ -76,9 +93,52 @@ const initInkGame =() => {
     });
 }
 
+// DOORS INTERACTION
+const initDoorInteraction = () => {
+    if (!closedDoor || !openDoor) return;
 
+    closedDoor.addEventListener("click", knockHandler);
+    closedDoor.addEventListener("touchstart", knockHandler);
+};
+const knockHandler = () => {
+    tapCount++;
+    
+    if (tapCount === 1) {
+        tapTimer = setTimeout(() => {
+            tapCount = 0;
+        }, 400);
 
+        wiggleDoor(closedDoorImg);
+    }
+
+    if (tapCount === 2) {
+        clearTimeout(tapTimer);
+        tapCount = 0;
+
+        closedDoor.style.display = "none";
+        openDoor.style.display = "block";
+
+        gsap.fromTo(openDoor, {
+            scale: 0.9,
+            opacity: 0
+        }, {
+            scale: 1,
+            opacity: 1,
+            duration: 0.6,
+            ease: "power2.out",
+            onComplete: () => {
+                transitionIntoCh5();
+            }
+        });
+    }
+};
 // GSAP
+const  animateIfVisible = (el, config) =>{
+    if (el && el.offsetParent !== null) {
+        gsap.from(el, config);
+    }
+}
+
 const bubbles = ()=>{
     gsap.utils.toArray('.bubble').forEach((bubble, i) => {
         gsap.from(bubble, {
@@ -96,7 +156,6 @@ const bubbles = ()=>{
         });
     });
 }
-
 const lines = () => {
     gsap.utils.toArray('.lineText').forEach((lineText, i) => {
         gsap.fromTo(lineText,
@@ -117,6 +176,7 @@ const lines = () => {
     });
 };
 
+// HERO
 const heroAnimation = ()=>{
     const tl = gsap.timeline({ defaults: { ease: "power2.out" } });
 
@@ -172,6 +232,7 @@ const heroAnimation = ()=>{
     return tl;
 }
 
+// CHAPTER 1
 const ch1 =()=>{
     gsap.from(".ch1", {
         y: '50%',
@@ -272,7 +333,6 @@ const unionAnimation = ()=>{
 
     return tl;
 }
-
 const cupidAnimation = ()=>{
     gsap.to(".img_arrow", {
         x: '-120vw',
@@ -284,6 +344,163 @@ const cupidAnimation = ()=>{
         },
         duration: 2,
     });
+}
+
+// CHAPTER 4
+const infoBible = () => {
+    gsap.utils.toArray(".ch4__listItem").forEach((item, i) => {
+        const number = item.querySelector(".ch4_number");
+        const text = item.querySelector(".ch4_infoText");
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: item,
+                start: "top 85%",
+                toggleActions: "restart none none none"
+            },
+            delay: i * 0.2 
+        });
+
+        tl.from(item, {
+            x: "-3rem",
+            opacity: 0,
+            duration: 0.4,
+            ease: "power2.out"
+        })
+
+        .from(number, {
+            opacity: 0,
+            y: "1rem",
+            duration: 0.4,
+            ease: "power2.out"
+        }, "-=0.2")
+
+        .from(text, {
+            opacity: 0,
+            y: "1rem",
+            duration: 0.4,
+            ease: "power2.out"
+        }, "-=0.3");
+    });
+};
+const bibleImg = () =>{
+    animateIfVisible(document.querySelector(".img_book"), {
+        xPercent: 50,
+        opacity: 0,
+        duration: 0.6,
+        ease: "power2.out",
+        scrollTrigger: {
+            trigger: ".ch4__text",
+            start: "center 50%",
+            toggleActions: "restart none none none"
+        }
+    });
+
+    animateIfVisible(document.querySelector(".img_elephantComplete"), {
+        xPercent: 50,
+        opacity: 0,
+        duration: 0.6,
+        ease: "power2.out",
+        scrollTrigger: {
+            trigger: ".ch4__text",
+            start: "center 50%",
+            toggleActions: "restart none none none"
+        }
+    });
+}
+const ctaCh4 = ()=>{
+    animateIfVisible(document.querySelector(".ch4_CTABigger"), {
+        x: "-3rem",
+        opacity: 0,
+        duration: 0.8,
+        ease: "power2.out",
+        scrollTrigger: {
+            trigger: ".ch4__section1",
+            start: "bottom 10%",
+            toggleActions: "play none none none"
+        }
+    });
+
+    animateIfVisible(document.querySelector(".ch4_CTAMobile"), {
+        y: "2rem",
+        opacity: 0,
+        duration: 0.6,
+        ease: "power2.out",
+        scrollTrigger: {
+            trigger: ".ch4_CTAMobile",
+            start: "top 85%",
+            toggleActions: "play none none none"
+        }
+    });
+}
+
+// CHAPTER 5
+const wiggleDoor = (element) => {
+    gsap.fromTo(
+        element,
+        { x: "-0.4rem" },
+        {
+            x: "0.4rem",
+            duration: 0.08,
+            repeat: 5,
+            yoyo: true,
+            ease: "power1.inOut",
+            overwrite: true,
+            clearProps: "x"
+        }
+    );
+};
+
+const transitionIntoCh5 = () => {
+    const titleCh5 = document.querySelector(".ch5__title1");
+    const bubble = document.querySelector(".bubble--door");
+    const doorGroup = document.querySelector(".ch5__doors");
+
+    setDisplayResponsive(sectionP1);
+    crownImg.style.display = "block";
+    textBlock.style.display = "block";
+
+    const tl = gsap.timeline();
+    tl.to([titleCh5, bubble], {
+        opacity: 0,
+        duration: 0.3,
+        onComplete: () => {
+            titleCh5.style.display = "none";
+            bubble.style.display = "none";
+        }
+    })
+    .to(openDoor, {
+        scale: 4,
+        opacity: 0,
+        duration: 1.4,
+        ease: "power2.out",
+        transformOrigin: "center center",
+    }, "<")
+    
+    .to(doorGroup, {
+        opacity: 0,
+        duration: 0.4,
+        onComplete: () => {
+            doorGroup.style.display = "none";
+        }
+    }, "-=0.6")
+
+    .fromTo([sectionP1, crownImg, textBlock],
+        { opacity: 0, y: 20 },
+        {
+            opacity: 1,
+            y: 0,
+            duration: 1,
+            ease: "power2.out",
+            stagger: 0.2,
+        }
+    );
+};
+
+const hideWithJs=()=>{
+    crownImg.style.display = "none";
+    textBlock.style.display = "none";
+    sectionP1.style.display = "none";
 }
 
 const init = () =>{
@@ -298,9 +515,14 @@ const init = () =>{
         titleUnion();
         unionAnimation();
         cupidAnimation();
+        infoBible();
+        bibleImg();
+        ctaCh4();
     });
 
+    hideWithJs();
     initInkGame();
+    initDoorInteraction();
 }
 
 init();
